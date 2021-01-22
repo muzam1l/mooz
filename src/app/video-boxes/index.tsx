a87b225b36a2eb2aa117d837e73029f6b415e77c
muzam <muzamilsofi@outlook.com>
2021-01-22 12:47:40 +0530

    user media layout changes

--- src/app/video-boxes/index.tsx
+++ src/app/video-boxes/index.tsx
@@ -1,58 +1,78 @@
 import { FunctionComponent } from 'react'
-import { mergeStyles, Stack, useTheme } from '@fluentui/react'
+import { Stack, Modal, ContextualMenu } from '@fluentui/react'
 import { useRecoilValue } from 'recoil'
+import { container, containerInner, userMediaContainer, displayMediaContainer, modalStyles } from './styles'
 import useSize from '../../utils/hooks/use-video-size'
 import { userStreamState, displayStreamState, remoteStreamsState } from '../../atoms'
 import VideoBox from '../../comps/video'
 
-const container = mergeStyles({
-    height: 'calc(100vh - 40px)',
-    display: 'flex',
-    overflowY: 'auto',
-})
-const containerInner = mergeStyles({
-    display: 'flex',
-    margin: 'auto',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-})
-
 const AR = 4 / 3
 
 const VideoBoxes: FunctionComponent = () => {
-    const theme = useTheme()
+    // const theme = useTheme()
 
     const userMedia = useRecoilValue(userStreamState)
     const displayMedia = useRecoilValue(displayStreamState)
     const remoteStreams = useRecoilValue(remoteStreamsState).map(s => s.stream)
 
-    let count = remoteStreams.length
-    if (userMedia) count += 1
-    if (displayMedia) count += 1
+    const count = remoteStreams.length
     const { x, y } = useSize(count, AR)
 
     return (
         <div className={container}>
             <div className={containerInner}>
-                {[userMedia, displayMedia, ...remoteStreams].filter(Boolean).map(
-                    stream =>
-                        stream && (
-                            <Stack
-                                key={stream.id}
-                                style={{
-                                    height: y,
-                                    maxWidth: x,
-                                    backgroundColor: theme.palette.neutralLight,
-                                    border: `1px solid ${theme.palette.white}`,
-                                }}
-                            >
-                                <VideoBox muted={stream.id === userMedia?.id} stream={stream} />
-                            </Stack>
-                        ),
-                )}
+                {remoteStreams.map(stream => (
+                    <Stack
+                        key={stream.id}
+                        style={{
+                            height: y,
+                            maxWidth: x,
+                            // backgroundColor: theme.palette.neutralLight,
+                        }}
+                    >
+                        <VideoBox stream={stream} />
+                    </Stack>
+                ))}
             </div>
+
+            <div className={userMediaContainer} id="user-media-container" />
+            <div className={displayMediaContainer} id="display-media-container" />
+            {displayMedia && (
+                <Modal
+                    isBlocking
+                    isModeless
+                    isOpen
+                    styles={modalStyles}
+                    layerProps={{
+                        hostId: 'display-media-container',
+                    }}
+                    dragOptions={{
+                        moveMenuItemText: 'Move',
+                        closeMenuItemText: 'Close',
+                        menu: ContextualMenu,
+                    }}
+                >
+                    <VideoBox muted stream={displayMedia} />
+                </Modal>
+            )}
+            {userMedia && (
+                <Modal
+                    isBlocking
+                    isModeless
+                    isOpen
+                    styles={modalStyles}
+                    layerProps={{
+                        hostId: 'user-media-container',
+                    }}
+                    dragOptions={{
+                        moveMenuItemText: 'Move',
+                        closeMenuItemText: 'Close',
+                        menu: ContextualMenu,
+                    }}
+                >
+                    <VideoBox muted stream={userMedia} />
+                </Modal>
+            )}
         </div>
     )
 }
---
+++ src/app/video-boxes/styles.ts
@@ -0,0 +1,43 @@
+import { IModalStyles, mergeStyles } from '@fluentui/react'
+
+const AR = 4 / 3
+
+export const container = mergeStyles({
+    height: 'calc(100vh - 40px)',
+    display: 'flex',
+    overflowY: 'auto',
+})
+export const containerInner = mergeStyles({
+    display: 'flex',
+    margin: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+})
+
+export const mediaContainer = mergeStyles({
+    position: 'fixed',
+    bottom: 0,
+    height: 200 / AR,
+    width: 200,
+})
+
+export const userMediaContainer = mergeStyles(mediaContainer, {
+    right: 0,
+})
+export const displayMediaContainer = mergeStyles(mediaContainer, {
+    right: 200,
+})
+
+export const modalStyles: Partial<IModalStyles> = {
+    main: {
+        minHeight: 100,
+        minWidth: 100,
+        height: '100%',
+        maxHeight: '100%',
+        width: '100%',
+        maxWidth: '100%',
+        display: 'flex',
+    },
+}
